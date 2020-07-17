ad2e30e8197ff22b5c7c531da569296d205867cb
Torreythms <torreythms@gmail.com>
2020-07-17 13:33:14 -0400

    pushing updates to the home page

--- mylotus/src/Home/Home.js
+++ mylotus/src/Home/Home.js
@@ -7,7 +7,7 @@ import lotusimg from '../images/nav-lotus-flower.png';
 function Home() {
     return(
         <div id="home"> 
-          <img className="home-flower" src={lotusimg}></img>
+          <img className="home-flower grow" src={lotusimg}></img>
             <div className="home-content-ctn">
             <h1 className="lotus-lingo"> Experience</h1>
             <h1 className="lotus-lingo"> Everything</h1>
@@ -15,6 +15,20 @@ function Home() {
             </div>
 
 <h2 className="home-btm-txt"> Music isn't just what we listen to, it’s who we are... </h2>
+
+<div className="icon-section">
+  <div className="home-flex-container">
+    <div>
+      <h1> Share and Discover Music </h1>
+      <p> Share your favorite music with your favorite people and listen to songs shared by them, all with the tap of a button. </p>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</div>
+
+
         </div>
 
 
