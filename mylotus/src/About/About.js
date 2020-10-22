02be8184569eb4c843bd4de5ea8cb2043ae4a9ab
cbrandt6 <cbrandt6@ycp.edu>
2020-10-22 09:32:14 -0400

    Getting basic layout setup

--- mylotus/src/About/About.js
+++ mylotus/src/About/About.js
@@ -1,20 +1,43 @@
 import React from 'react'
+import {Container, Image} from 'react-bootstrap';
 import './About.css'
 import { Link } from "react-router-dom";
+const flower =  require("../images/nav-lotus-flower.png");
 
 
+class About extends React.Component{
+    render(){
+        return(
 
-const About = () => {
-return(
+            <div id="About-section">
+                <Container fluid id="About-Lotus">
+                    <h1>About Lotus</h1>
+                    <Image id="Top-Flower" src={flower}></Image>
+                </Container>
 
-<div id="About-section">
+                <div id="The-Mission">
 
-    
+                </div>
 
-       
+                <div id="The-Vision">
 
-    </div>
-)
+                </div>
+
+                <div id="The-Story">
+                    
+                </div>
+
+                <div id="The-Name">
+
+                </div>
+
+                <div id="The-Team">
+                
+                </div>
+
+            </div>
+        )
+    }    
 }
 
 export default About;
