a44d0e633b11ea094ce655dee3ce0915863c0b19
cbrandt6 <cbrandt6@ycp.edu>
2020-10-29 09:51:04 -0400

    Working on formatting text

--- mylotus/src/About/About.js
+++ mylotus/src/About/About.js
@@ -9,7 +9,7 @@ class About extends React.Component{
     render(){
         return(
 
-            <div id="aboutSection">
+            <div id="aboutPage">
 
                 <div id="aboutLotus">
 
@@ -24,8 +24,6 @@ class About extends React.Component{
                         </div>
 
                     </div>
-
-
                     <p>
                         Our goal is to disrupt the traditional social media landscape of just sharing pictures, 
                         ideas, and videos to sharing something more meaningful: Live Experiences.
@@ -33,41 +31,47 @@ class About extends React.Component{
                     
                 </div>
                 
-                <div id="theMission">
+                <div id="theMission" class="aboutSection">
                     <span class="aboutHeader">The Mission</span>
                     <p>
-                        To create a platform where users can discover, experience, and share all of their favorite music from a w
-                        ide range of services in one location.
+                        To create a platform where users can discover, experience, and share all of their favorite music from a wide range of services in one location.
                     </p>
                 </div>
 
-                <div id="theVision">
+                <div id="theVision" class="aboutSection">
                     <span class="aboutHeader">The Vision</span>
                     <p>
                         A world united through music.
                     </p>
                 </div>
 
-                <div id="theStory">
+                <div id="theStory" class="aboutSection">
                     <span class="aboutHeader">The Story</span>
-                    <p>
-                        More and more these days, people are trying to share music with their friends on social media. This normally comes in the form of a screenshot on Instagram or Snapchat or a link on Facebook or Twitter. While this is great for sharing, people looking to listen to the song, add it to a playlist, or share with their friends must go to a different service and find it themselves.
-                    </p>
-                    <p>
-                        Furthermore, with the streaming market being split between Apple Music, Spotify, and a list of other streaming services, users can only interact with other users of the same service. And thus Lotus was born. Lotus is a social network platform dedicated to discovering, experiencing, and sharing music.
-                    </p>
+                    <div id="theStoryTextContainer">
+                        <p>
+                            More and more these days, people are trying to share music with their friends on social media. This normally comes in the form of a screenshot on Instagram or Snapchat or a link on Facebook or Twitter. While this is great for sharing, people looking to listen to the song, add it to a playlist, or share with their friends must go to a different service and find it themselves.
+                        </p>
+                        <p>
+                            Furthermore, with the streaming market being split between Apple Music, Spotify, and a list of other streaming services, users can only interact with other users of the same service. And thus Lotus was born. Lotus is a social network platform dedicated to discovering, experiencing, and sharing music.
+                        </p>
+                    </div>
+                    
+                    
                 </div>
 
-                <div id="theName">
+                <div id="theName" class="aboutSection">
                     <span class="aboutHeader">The Name</span>
-                    <p>We chose the lotus flower as our core symbol for a few reasons: </p>
-                    <p>Lotus ends in the word “us,” emphasizing the social nature of the platform.</p>
-                    <p>In eastern religions, the lotus flower represents rebirth and enlightenment, which is what we aim to do to with music sharing.</p>
-                    <p>As a water dwelling flower, the lotus can compliment a stream, similar to how we compliment streaming services.</p>
-                    <p>Despite only growing in the mud, the lotus flower blooms into something beautiful, representing how we will continue to grow despite any challenges that come our way.</p>
+                    <div id="theNameTextContainer">
+                        <p>We chose the lotus flower as our core symbol for a few reasons: </p>
+                        <p>Lotus ends in the word “us,” emphasizing the social nature of the platform.</p>
+                        <p>In eastern religions, the lotus flower represents rebirth and enlightenment, which is what we aim to do to with music sharing.</p>
+                        <p>As a water dwelling flower, the lotus can compliment a stream, similar to how we compliment streaming services.</p>
+                        <p>Despite only growing in the mud, the lotus flower blooms into something beautiful, representing how we will continue to grow despite any challenges that come our way.</p>
+                    </div>
+                    
                 </div>
 
-                <div id="theTeam">
+                <div id="theTeam" class="aboutSection">
                     <span class="aboutHeader">The Team</span>
                 </div>
 
