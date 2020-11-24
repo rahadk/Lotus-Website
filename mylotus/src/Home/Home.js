fcfad7d22a86f73cb922818b6476ff0bf86cc51d
Walrussuit101 <tjefferson@ycp.edu>
2020-11-23 22:53:17 -0500

    add cross platform section

--- mylotus/src/Home/Home.js
+++ mylotus/src/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.css'
-import { Link } from "react-router-dom";
 import flower from '../images/nav-lotus-flower.png';
+import Footer from "../Footer/Footer";
 
 
 function Home() {
@@ -23,6 +23,13 @@ function Home() {
                 <p>Music isn't just what we listen to, it’s who we are...</p>
                 <div id="downloadBox"></div>
             </div>
+            <div id="crossPlatform">
+                <b><i><p id="crossPlatformHeading">Cross-Platform Streaming</p></i></b>
+                <p id="crossPlatformText">
+                    Sync your streaming service and listen in one place with Lotus.
+                </p>
+            </div>
+            <Footer></Footer>
         </div>
     );
 }
