c0276b982f8288f764f7b4590442348ecc1c0870
Walrussuit101 <tjefferson@ycp.edu>
2020-11-29 12:19:32 -0500

    Updates

---
+++ precache-manifest.e3f9e6084bd60b44c774646eee26e0fb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697f640729154478c115814c0cffac13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc6cf9f12a89e5e7b5d4",
+    "url": "/static/css/2.cfdd86ab.chunk.css"
+  },
+  {
+    "revision": "85b0510a8de9341394a5",
+    "url": "/static/css/main.7ba1a6bb.chunk.css"
+  },
+  {
+    "revision": "cc6cf9f12a89e5e7b5d4",
+    "url": "/static/js/2.9bc8b887.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.9bc8b887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b0510a8de9341394a5",
+    "url": "/static/js/main.608fbebe.chunk.js"
+  },
+  {
+    "revision": "7980d5e77531bcdd3926",
+    "url": "/static/js/runtime-main.9c4a9a46.js"
+  },
+  {
+    "revision": "2445149627b83b0229bdee91f4a94398",
+    "url": "/static/media/apple-music.24451496.png"
+  },
+  {
+    "revision": "56ef95efecd0d8c8e69ffa9a6556a2fb",
+    "url": "/static/media/ben_cropped.56ef95ef.jpg"
+  },
+  {
+    "revision": "c9fc3e9d9af379866d983b0b3e3fde78",
+    "url": "/static/media/nav-lotus-flower.c9fc3e9d.png"
+  },
+  {
+    "revision": "dea93227e2b3e473a80db80174b9ef30",
+    "url": "/static/media/navbar-lotus-logo.dea93227.png"
+  },
+  {
+    "revision": "82717d893c42ca46c9769983d92c4d09",
+    "url": "/static/media/navbar-lotus-sun.82717d89.png"
+  },
+  {
+    "revision": "2fafacf57367437680e3ceb8b74a22b7",
+    "url": "/static/media/rahad_cropped.2fafacf5.jpg"
+  },
+  {
+    "revision": "2d83988d35dcbe14d3a15e79c3aab2ce",
+    "url": "/static/media/share-discover-phone-highres.2d83988d.png"
+  },
+  {
+    "revision": "f82554647b9cb23cc358aad727ea3884",
+    "url": "/static/media/show-off-phone-highres.f8255464.png"
+  },
+  {
+    "revision": "148b28a3992349e8db92184c65d24bbd",
+    "url": "/static/media/spotify.148b28a3.png"
+  }
+]);
